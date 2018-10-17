327059e9a1db88612d1e38f1c183e409cb316716
mcewenal <mcewenal@mail.uc.edu>
2018-10-17 16:30:37 -0400

    start profile page

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
+//CardView setup
+import { registerElement } from 'nativescript-angular/element-registry';
+import { CardView } from 'nativescript-cardview';
+registerElement('CardView', () => CardView);
+
 @Component({
     selector: "Groups",
     moduleId: module.id,
