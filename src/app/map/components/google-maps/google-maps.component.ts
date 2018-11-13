82e734a4b7a1c892a185c891c738661c69893bad
kaidong nie <kdnie20@gmail,com>
2018-11-13 01:15:55 -0500

    separate tag generation into a service

--- src/app/map/components/google-maps/google-maps.component.ts
+++ src/app/map/components/google-maps/google-maps.component.ts
@@ -1,9 +1,7 @@
+import { ITag } from './../../../interfaces/tag.interfaces';
 import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 import {registerElement} from "nativescript-angular/element-registry";
 import { MapView, Marker, Position } from 'nativescript-google-maps-sdk';
-
-import { Image } from "ui/image";
-import { ImageSource } from "image-source";
 
 // Important - must register MapView plugin in order to use in Angular templates
 registerElement("MapView", () => MapView);
@@ -28,27 +26,45 @@ export class GoogleMapsComponent implements OnInit {
 
       this.mapView = event.object;
 
-      var imageSource: ImageSource = new ImageSource;
-      imageSource.fromResource("custommarker");
-      let image = new Image();
-      image.imageSource = imageSource;
-
-      var markers:any[] = [
-        {
-          "lat": 39.124173,
-          "long": -84.516864,
-          "title": "Here",
-          "icon": image
-        }
-      ];
-
-      markers.forEach(m => {
-        var marker = new Marker();
-        marker.position = Position.positionFromLatLng(m.lat, m.long);
-        marker.title = m.title;
-        marker.icon = m.icon;
-        this.mapView.addMarker(marker);
-      });
+      this.generateTags(this.mapView, this.tags);
     };
 
+    generateTags = (mapView: MapView, tags: any[]) => {
+
+    };
+
+
+    tags = [
+      {
+        type: 'text',
+        position: Position.positionFromLatLng(39.128050, -84.523169),
+        upvotes: 12,
+        downvotes: 2
+      }, {
+        type: 'video',
+        position: Position.positionFromLatLng(39.128150, -84.522769),
+        upvotes: 4,
+        downvotes: 2
+      }, {
+        type: 'image',
+        position: Position.positionFromLatLng(39.128450, -84.523169),
+        upvotes: 7,
+        downvotes: 1
+      }, {
+        type: 'audio',
+        position: Position.positionFromLatLng(39.128350, -84.523069),
+        upvotes: 102,
+        downvotes: 24
+      }, {
+        type: 'video',
+        position: Position.positionFromLatLng(39.128125, -84.523269),
+        upvotes: 12,
+        downvotes: 2
+      }, {
+        type: 'text',
+        position: Position.positionFromLatLng(39.128150, -84.522569),
+        upvotes: 11,
+        downvotes: 0
+      }, 
+    ]
 }
