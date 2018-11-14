ea86d5d77435c855950018559f0b433fe7257e88
unknown <kdnie20@gmail,com>
2018-11-14 11:03:33 -0500

    add icons for the markers

--- src/app/map/services/map-tag.service.ts
+++ src/app/map/services/map-tag.service.ts
@@ -1,17 +1,24 @@
+import { Image } from 'ui/image';
+import { ImageSource } from 'image-source';
 import { MapView, Marker } from 'nativescript-google-maps-sdk';
 import { Injectable } from '@angular/core';
 import { ITag } from '~/app/interfaces/tag.interfaces';
-import { fromResource } from 'tns-core-modules/image-source/image-source';
 
 @Injectable()
 export class MapTagService {
 
     public generateMapTag = (mapView: MapView, tags: ITag[]) => {
         tags.forEach(tag => {
+            let imageSource: ImageSource = new ImageSource;
+            imageSource.fromResource('map+marker' + tag.type);
+            let image = new Image();
+            image.imageSource = imageSource;
+
             let marker = new Marker();
             marker.position = tag.position;
             marker.title = tag.title;
-            marker.icon = 'marker_icon_' + tag.type;
+            marker.icon = image;
+            mapView.addMarker(marker);
 
         })
     }
